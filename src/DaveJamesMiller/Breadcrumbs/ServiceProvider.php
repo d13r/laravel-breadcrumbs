b71539f1cbf3adaad36008f543de90f73557e4c9
Dave James Miller <dave@davejamesmiller.com>
2014-10-26 22:03:43 +0000

    Load app/Http/breadcrumbs.php in Laravel 5.0

--- src/DaveJamesMiller/Breadcrumbs/ServiceProvider.php
+++ src/DaveJamesMiller/Breadcrumbs/ServiceProvider.php
@@ -49,8 +49,12 @@ class ServiceProvider extends BaseServiceProvider
         // Register the package so the default view can be loaded
         $this->package('davejamesmiller/laravel-breadcrumbs');
 
-        // Load the app breadcrumbs if they're in app/breadcrumbs.php
-        if (file_exists($file = $this->app['path'].'/breadcrumbs.php'))
+        // Load the app breadcrumbs if they're in app/Http/breadcrumbs.php (Laravel 5.x)
+        if (file_exists($file = $this->app['path'].'/Http/breadcrumbs.php'))
+            require $file;
+
+        // Load the app breadcrumbs if they're in app/breadcrumbs.php (Laravel 4.x)
+        elseif (file_exists($file = $this->app['path'].'/breadcrumbs.php'))
             require $file;
     }
 }
