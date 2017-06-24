d15a56e23637f38113c5fbbbf34542a96fa46181
Dave James Miller <dave@davejamesmiller.com>
2017-06-24 01:58:29 +0100

    Move phpunit.php to test/bootstrap.php

--- phpunit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Load Composer packages
-require __DIR__ . '/vendor/autoload.php';
-
-// Load base class
-require __DIR__ . '/tests/TestCase.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+// Load Composer packages
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load base class
+require __DIR__ . '/../tests/TestCase.php';
