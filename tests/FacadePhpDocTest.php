e9e214e6a9540094e61faed1ad4c800b320a83b8
Dave James Miller <dave@davejamesmiller.com>
2018-07-26 21:16:58 +0100

    Revert "Add unit test for FQCN in Facade (#187) and refactor"
    This reverts commit b9e3afce1e4e9674d693b7d58d9ad983eedde98f.

--- tests/FacadePhpDocTest.php
+++ tests/FacadePhpDocTest.php
@@ -4,34 +4,21 @@ namespace BreadcrumbsTests;
 
 class FacadePhpDocTest extends TestCase
 {
-    private const TAG_REGEX = '/
+    public function tags()
+    {
+        $code = file_get_contents(__DIR__ . '/../src/BreadcrumbsManager.php');
+
+        $pattern = '/
             \*
             \s+
-            @(?:param|return|throws|see)    # Tag
+            @(?:param|return|throws)
             \s+
-            (.+?)                           # Value
+            (.+?)
             \s
             .*
         /x';
 
-    private const ALLOWED_TYPE_REGEX = '/
-        ^
-        (
-            # Fully qualified class
-            \\\\.*
-            # Self-referencing class
-            |self(::.+)?
-            # Built-in types
-            |array|bool|callable|int|mixed|null|string|void
-        )
-        $
-    /x';
-
-    public function tagsInFacade()
-    {
-        $code = file_get_contents(__DIR__ . '/../src/Facades/Breadcrumbs.php');
-
-        preg_match_all(static::TAG_REGEX, $code, $matches, PREG_SET_ORDER);
+        preg_match_all($pattern, $code, $matches, PREG_SET_ORDER);
 
         $tags = [];
         foreach ($matches as $match) {
@@ -42,40 +29,13 @@ class FacadePhpDocTest extends TestCase
         }
     }
 
-    /** @dataProvider tagsInFacade */
-    public function testFullyQualifiedClassNamesInFacade($class, $line)
-    {
-        // IDE Helper (v2.4.3) doesn't rewrite class names to FQCNs, so make sure only
-        // fully qualified class names and built-in types are used in the Facade class
-        $this->assertRegExp(
-            static::ALLOWED_TYPE_REGEX,
-            $class,
-            "Must use fully qualified class names in Breadcrumbs Facade PhpDoc: $line"
-        );
-    }
-
-    public function tagsInManager()
-    {
-        $code = file_get_contents(__DIR__ . '/../src/BreadcrumbsManager.php');
-
-        preg_match_all(self::TAG_REGEX, $code, $matches, PREG_SET_ORDER);
-
-        $tags = [];
-        foreach ($matches as $match) {
-            foreach (explode('|', $match[1]) as $class) {
-                // Return the whole line too so it can be seen in the error message
-                yield [$class, $match[0]];
-            }
-        }
-    }
-
-    /** @dataProvider tagsInManager */
-    public function testFullyQualifiedClassNamesInManager($class, $line)
+    /** @dataProvider tags */
+    public function testFullyQualifiedClassNames($class, $line)
     {
         // IDE Helper (v2.4.3) doesn't rewrite class names to FQCNs, so make sure only
         // fully qualified class names and built-in types are used in the Manager class
         $this->assertRegExp(
-            static::ALLOWED_TYPE_REGEX,
+            '/^(\\\\.*|array|bool|callable|int|mixed|null|string|void)$/',
             $class,
             "Must use fully qualified class names in BreadcrumbsManger PhpDoc: $line"
         );
