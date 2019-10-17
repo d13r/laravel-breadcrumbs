6acaadbaaf2ce78de6ffc15c17ec0e17fda8e54b
Alexandr Chernyaev <bliz48rus@gmail.com>
2019-10-17 23:23:28 +0300

    Added phpdoc for facade Breadcrumbs
    Hi, this is a great package, but I think we can improve the experience with it.
    
    In order for the IDE to have tips, you need to add a few comments, which will greatly facilitate life.
    In the same way they solve the problem in laravel itself.
    For example:
    https://github.com/laravel/framework/blob/f61b787d88505d94d4ca691d43307518aed3dbd9/src/Illuminate/Support/Facades/Log.php#L5-L20
    
    This PR adds comments to the main methods.

--- src/Facades/Breadcrumbs.php
+++ src/Facades/Breadcrumbs.php
@@ -8,6 +8,19 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Breadcrumbs facade - allows easy access to the Manager instance.
  *
+ * @method static void for(string $page, callable $callback)
+ * @method static void register(string $name, callable $callback)
+ * @method static void before(callable $callback)
+ * @method static void after(callable $callback)
+ * @method static bool exists(string|null $name)
+ * @method static \Illuminate\Support\Collection generate(string|null $name, ...$params)
+ * @method static \Illuminate\Support\HtmlString view(string $view, string $name, ...$params)
+ * @method static \Illuminate\Support\HtmlString render(string $name, ...$params)
+ * @method static \stdClass|null current()
+ * @method static array getCurrentRoute()
+ * @method static void setCurrentRoute(string $name, ...$params)
+ * @method static void clearCurrentRoute()
+ *
  * @see BreadcrumbsManager
  */
 class Breadcrumbs extends Facade
